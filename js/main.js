0e643cb781c6f0a16c0de9ab9f9dcdf42c740d6b
bfoley-teamug <withtechron@gmail.com>
2017-08-31 15:19:43 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Go Here: http://git.blivesta.com/animsition/ 
+
+$(".animsition").animsition({
+  inClass: 'fade-in-right-lg',
+  outClass: 'fade-out-right-lg', 
+  linkElement: ' header a', 
+  inDuration: 1000,
+  outDuration: 500
+});  
